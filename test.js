43ffa6577a621b855e4169418763f267d276e1d4
ced404 <ced@404err.com>
2015-02-06 23:09:48 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
 simply.title('Hello World!');
 simply.vibe('short');
+
+// click
+simply.on('singleClick', function(e) {
+  simply.subtitle('You pressed the ' + e.button + ' button!');
+});
+
+// long click
+simply.on('longClick', function(e) {
+  simply.subtitle('You held the ' + e.button + ' button!');
+});
+
+simply.on('accelTap', function(e) {
+  simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+});
