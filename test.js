16d41a295a214ba48db9bd8ac339a0e097b4c126
ced404 <ced@404err.com>
2015-02-06 23:13:36 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,26 @@
 simply.title('Hello World!');
 simply.vibe('short');
 
+
+function geolocate () {
+  navigator.geolocation.getCurrentPosition(function(pos) {
+    var coords = pos.coords;
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+        'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+    ajax({ url: weatherUrl, type: 'json' }, function(data) {
+      simply.text({ title: data.name, subtitle: data.main.temp });
+    });
+  });
+}
+
+
+
 // click
 simply.on('singleClick', function(e) {
+  
   simply.subtitle('You pressed the ' + e.button + ' button!');
+  
+  geolocate();
 });
 
 // long click
